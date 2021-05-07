6db0cf425eeab44a9b57f896a15db807e9eb121d
mrkrusher2000 <70381354+mrkrusher2000@users.noreply.github.com>
2021-05-07 23:25:35 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def index():
+    print("kdfnlfk")
+    return jsonify({'greetings': 'Python is running'})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
