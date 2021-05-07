d6d93ba5e7d1dd07d8850fdcd1c5a27d24894b6f
mrkrusher2000 <70381354+mrkrusher2000@users.noreply.github.com>
2021-05-07 23:45:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET'])
 def index():
     print("kdfnlfk")
-    return jsonify({'greetings': 'Python is running'})
+    return jsonify({"greetings": "Phew! It's Working"})
 
 
 if __name__ == "__main__":
